8c4270d6d8975545cbf9388539493ec5537d535c
zcdhp <zcdhp@nocontact.com>
2025-10-01 15:41:45 +0800

    update pixiv user name locater

--- pixivPicName.js
+++ pixivPicName.js
@@ -10,22 +10,23 @@
 // ==/UserScript==
 
 (() => {
-  // pixiv 「enden」 愛城華恋 101181889 - 1
-  const main = () => {
-    const path = window.location.pathname;
-    const artId = path.split("/").filter(x => x !== "")[1]
-    const userName = document.querySelector("a[href^=\"/users/\"] div").title;
-    const picName = document.querySelector("h1").innerHTML;
-    const picNumEle = document.querySelector("div[role='presentation'] .gtm-manga-viewer-preview-modal-open span");
-    const picNum = picNumEle ? picNumEle.innerHTML.split("/")[0] : null;
+	// pixiv 「enden」 愛城華恋 101181889 - 1
+	const main = () => {
+		const path = window.location.pathname;
+		const artId = path.split("/").filter(x => x !== "")[1]
+		const userName = document.querySelector("div[aria-haspopup=\"true\"] a[href^=\"/users/\"] div").title
+		const picName = document.querySelector("h1").innerHTML;
+		const picNumEle = document.querySelector("div[role='presentation'] .gtm-manga-viewer-preview-modal-open span");
+		const picNum = picNumEle ? picNumEle.innerHTML.split("/")[0] : null;
 
 
-    const text = `pixiv 「${userName}」 ${picName} ${artId}${picNum ? ` - ${picNum}` : ""}`
-    navigator.clipboard.writeText(text);
-  };
+		const text = `pixiv 「${userName}」 ${picName} ${artId}${picNum ? ` - ${picNum}` : ""}`
+		navigator.clipboard.writeText(text);
+	};
 
-  document.addEventListener("keydown", e => {
-    if (e.key == "q" && e.ctrlKey)
-      main();
-  })
+	document.addEventListener("keydown", e => {
+		if (e.key == "q" && e.ctrlKey) {
+			main();
+		}
+	})
 })()
